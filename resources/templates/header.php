042278b36719a070c47cfc1c103905a65d87090f
tim <timbog80@gmail.com>
2014-10-01 14:30:09 -0700

    add arrow down

--- resources/templates/header.php
+++ resources/templates/header.php
@@ -1,17 +1,20 @@
 <header>
   <div id="header-wrapper">
+    <div class="intro">
+      <div class="logo">
+        <a href="index.php">scribbble.io</a>
+      </div>
 
-    <div class="logo">
-      <a href="index.php">scribbble.io</a>
-    </div>
-
-    <div class="new-scrib">
-      <a href="#">new</a>
+      <div class="new-scrib">
+        <a href="#">new</a>
+      </div>
     </div>
 
     <div class="user">
       <a href="#">notifications<span class="notification">28</span></a>
-      <a class="username" href="#">tim bogdanov</a>
+      <a class="username" href="#">tim bogdanov
+        <i class="fa fa-angle-down"></i>
+      </a>
     </div>
   </div>
 </header>
