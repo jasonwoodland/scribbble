abd4693e6c287d57a40401af0a9626f4831d0165
tim <timbog80@gmail.com>
2014-10-01 19:03:35 -0700

    edit login page

--- login.php
+++ login.php
@@ -2,16 +2,7 @@
   <?php require_once 'resources/templates/head.php'; ?>
 
   <body>
-    <div id="login-wrapper">
-
-      <div class="login-logo">
-        <a href="index.php">scribbble.io</a>
-      </div>
-      <form action="#" method="#">
-        <input type="text" placeholder="username">
-        <input type="password" placeholder="password">
-      </form>
-    </div>
+    <?php require_once 'resources/templates/login-form.php'; ?>
   </body>
 
 </html>
---
+++ resources/templates/login-form.php
@@ -0,0 +1,12 @@
+<div id="login-wrapper">
+
+  <div class="login-logo">
+    <a href="index.php">scribbble.io</a>
+  </div>
+  
+  <form action="#" method="#">
+    <input type="text" placeholder="username">
+    <input type="password" placeholder="password">
+  </form>
+
+</div>
