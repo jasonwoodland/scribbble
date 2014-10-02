715544511b527dc317a2687d8189ccf51ea8b98d
tim <timbog80@gmail.com>
2014-10-01 17:37:40 -0700

    added login page

---
+++ login.php
@@ -0,0 +1,17 @@
+<html>
+  <?php require_once 'resources/templates/head.php'; ?>
+
+  <body>
+    <div id="login-wrapper">
+
+      <div class="login-logo">
+        <a href="index.php">scribbble.io</a>
+      </div>
+      <form action="#" method="#">
+        <input type="text" placeholder="username">
+        <input type="password" placeholder="password">
+      </form>
+    </div>
+  </body>
+
+</html>
