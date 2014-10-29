d2e6a8ae3efe9a046e552b4386734063a3cdbc2d
Tim <timbog80@gmail.com>
2014-10-28 17:30:11 -0700

    fix some bugs

--- resources/templates/editor-html-header.php
+++ resources/templates/editor-html-header.php
@@ -14,7 +14,7 @@
 	</div>
 
 	<ul class="user">
-		<li><a href="#" class="notification"><span>4</span></a></li>
+		<li><a href="#" class="notification"><span><i class="ion-ios7-bell"></i></span></a></li>
 		<li>
 			<a class="username" href="profile.php"><?=$me->profile(PDO::FETCH_OBJ)->username?>
 			<i class="ion-ios7-arrow-down"></i></a>
