33cbe207023cea1499fa77a331880c3b614f976d
Tim <timbog80@gmail.com>
2014-10-29 01:00:25 -0700

    small updates

--- resources/templates/editor-html-header.php
+++ resources/templates/editor-html-header.php
@@ -1,23 +1,23 @@
 <header>
 	<div id="header-wrapper">
 
+	<!-- logo and create new scrib -->
 	<div class="intro">
 		<div class="logo">
 			<a class="logo-anchor" href="feed.php">scribbble.io</a>
 		</div>
 
 		<div class="new-scrib">
-			<a href="#">save</a>
-			<a href="#">make private</a>
-			<a href="#">share</a>
+			<a href="../../editor.php">new <i class="ion-ios7-arrow-right"></i></a>
 		</div>
 	</div>
-
+	
+	<!-- user info in header -->
 	<ul class="user">
 		<li><a href="#" class="notification"><span><i class="ion-ios7-bell"></i></span></a></li>
+		<li><a href="settings.php" class="notification header-options"><span><i class="ion-gear-a"></i></span></a></li>
 		<li>
-			<a class="username" href="profile.php"><?=$me->profile(PDO::FETCH_OBJ)->username?>
-			<i class="ion-ios7-arrow-down"></i></a>
+			<a class="username" href="profile.php"><?=$me->profile(PDO::FETCH_OBJ)->username?></a>
 	 	</li>
 	 </ul> 
 
