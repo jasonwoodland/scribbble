29f4dba8127983e59a0a3a5806420ddc787f0250
Tim <timbog80@gmail.com>
2014-11-06 18:00:05 -0800

    fix header

--- resources/templates/editor-html-header.php
+++ resources/templates/editor-html-header.php
@@ -7,13 +7,8 @@
 			<a class="logo-anchor" href="/">scribbble.io</a>
 		</div>
 
-<<<<<<< HEAD
-		<div class="new-scrib" id="scribe-save">
-			<a href="#">save <i class="ion-ios7-arrow-right"></i></a>
-=======
 		<div class="new-scrib" id="save-scribe">
 			<a href="#">save</a>
->>>>>>> f021d9a091587110a5cee71c94e5e2d66e0a41e7
 		</div>
 	</div>
 	
