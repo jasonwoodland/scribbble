234719eed1e3be21521179b5be7c5d66715da99f
Tim <timbog80@gmail.com>
2014-11-07 19:00:37 -0800

    add heart, the counter will be on the scribe box

--- resources/templates/html-header.php
+++ resources/templates/html-header.php
@@ -4,7 +4,7 @@
 	<!-- logo and create new scrib -->
 	<div class="intro">
 		<div class="logo">
-			<a class="logo-anchor" href="/">scribbble.io</a>
+			<a class="logo-anchor" href="/">scribbble.io <span>beta</span></a>
 		</div>
 
 		<div class="new-scrib">
@@ -16,18 +16,7 @@
 	<ul class="user">
 		<li><a href="#" class="notification"><span><i class="ion-ios7-bell"></i></span></a></li>
 		<li>
-			<a class="username" href="/profile">jasonwoodland<?=$me->profile(PDO::FETCH_OBJ)->username?><i class="ion-ios7-arrow-down"></i></a>
-
-			<ul class="dropdown">
-				<div class="dropdown-profile-img">
-					<img src="#" alt="#">
-				</div>
-
-				<div class="dropdown-nav">
-					<li><a href="#">jasonwoodland</a></li>
-					<li class="scribe-count"><a href="#"><span class="number-scribes">4</span><br>scribes</a></li>
-				</div>
-			</ul>
+			<a class="username" href="/profile"><?=$me->profile(PDO::FETCH_OBJ)->username?><i class="ion-ios7-arrow-down"></i></a>
 	 	</li>
 	 </ul> 
 
