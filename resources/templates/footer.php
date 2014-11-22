e401f70d806a6015f4e37795fce201d2b425f070
Tim <timbog80@gmail.com>
2014-11-21 20:12:27 -0800

    add footer and hide x scroll

--- resources/templates/footer.php
+++ resources/templates/footer.php
@@ -1,6 +1,25 @@
+		<div class="moto">
+			<h3>a website for web designers.</h3>
+		</div>
+
 		<footer>
-			
-			
+			<div class="container12">
+				<div class="row">
+					<div class="column12">
+						<div class="copyright-float">
+							<a href="#">about</a>|
+							<p class="copyright">&copy; scribbble.io 2014. all rights reserved.</p>
+						</div>
+
+						<div class="connect">
+							<a target="blank" href="https://twitter.com/scribbble_io"><i class="ion-social-twitter"></i></a>
+							<a target="blank" href="https://www.facebook.com/scribbble.io"><i class="ion-social-facebook"></i></a>
+							<a target="blank" href="http://instagram.com/scribbble.io"><i class="ion-social-instagram"></i></a>
+						</div>
+						
+					</div>
+				</div>
+			</div>
 		</footer>
 
 		<?php defined('NO_HTML_FOOTER') or require 'html-footer.php' ?>
