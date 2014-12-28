27a6f6fc5ec3fa0e1eca65ebeac18122a80cd1a6
Jason Woodland <hi@jasonwoodland.com>
2014-12-28 18:48:26 +1100

    universalised html header.

---
+++ resources/templates/html-header-menu.php
@@ -0,0 +1,12 @@
+<ul class="user">
+	<li><a href="#" class="notification"><span><i class="ion-ios7-bell"></i></span></a></li>
+	<li>
+		<a class="username" href="#"><i class="ion-grid"></i></a>
+
+		<ul class="dropdown">
+			<li><a href="/<?=USERNAME?>">profile</a></li>
+			<li><a href="mailto: scribbble <contact@scribbble.io>">report bug</a></li>
+			<li><a href="/sign-out?session_id=<?=session_id()?>">sign out</a></li>
+		</ul>
+	</li>
+ </ul> 
--- resources/templates/view-scribe-html-header.php
+++ resources/templates/view-scribe-html-header.php
@@ -17,19 +17,7 @@
 	</div>
 	
 	<!-- user info in header -->
-	<ul class="user">
-		<li><a href="#" class="notification"><span><i class="ion-ios7-bell"></i></span></a></li>
-		<li>
-			<a class="username" href="#"><i class="ion-grid"></i></a>
-
-			<ul class="dropdown">
-				<li><a href="/<?=USERNAME?>">profile</a></li>
-				<li><a href="mailto:timbog80@gmail.com">report bug</a></li>
-				<li><a href="/sign-out?session_id=<?=session_id()?>">sign out</a></li>
-			</ul>
-	 	</li>
-	</ul> 
-
+   	<?php require 'html-header-menu.php'; ?>
    </div>
 
 </header>
